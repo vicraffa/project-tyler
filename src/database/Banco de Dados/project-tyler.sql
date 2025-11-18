df6c5679d8a9f03d13c45718acf9785c1c039564
vicraffa <victor.loliveira@sptech.school>
2025-11-18 07:42:21 -0300

    reestruturação nas pastas

---
+++ src/database/Banco de Dados/project-tyler.sql
@@ -0,0 +1,33 @@
+drop ddrop database projecttyler;
+create database projecttyler;
+use projecttyler;
+
+create table album (
+id int primary key auto_increment,
+nome varchar(45),
+descricao varchar(100),
+img char(1)
+);
+
+create table usuario (
+id int primary key auto_increment,
+nickname varchar(100),
+email varchar(100),
+senha varchar(45),
+album_id int,
+constraint albumUsuario foreign key (album_id) references album (id)
+);
+
+create table tentativaQuestionario (
+id int auto_increment,
+usuario_id int,
+primary key (id, usuario_id),
+constraint usuarioTentativaQuestionario foreign key (usuario_id) references usuario (id),
+pontuacao int,
+dtRegistro datetime default current_timestamp
+);
+
+insert into usuario (nickname, email, senha) values
+('victor', 'victor@email.com', 'Album@123');
+
+select * from usuario
