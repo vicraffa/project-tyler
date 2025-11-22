64918028fd6ce8588a1f07453517a782d3a5845c
vicraffa <victor.loliveira@sptech.school>
2025-11-22 17:57:09 -0300

    tabela chat atualizada

--- src/database/Banco de Dados/project-tyler.sql
+++ src/database/Banco de Dados/project-tyler.sql
@@ -20,13 +20,58 @@ erros int not null,
 dtRegistro datetime default current_timestamp
 );
 
-select * from usuario;
+create table chat(
+id int primary key auto_increment,
+mensagem varchar(200),
+usuario_id int,
+constraint fkUsuarioChat foreign key (usuario_id) references usuario(id)
+);
 
+insert into usuario (nickname, email, senha) values
+('jullya', 'jullya@email.com', 'Jullya@12345'),
+('akira', 'akira@email.com', 'Akira@12345'),
+('victor', 'victor@email.com', 'Victor@12345'),
+('nicole', 'nicole@email.com', 'Nicole@12345'),
+('arthur', 'arthur@email.com', 'Arthur@12345');
+
+insert into tentativaQuestionario(usuario_id, acertos, erros) values
+(1, 13, 17),
+(1, 25, 5),
+(2, 24, 6),
+(3, 10, 20),
+(4, 24, 6),
+(4, 5, 25),
+(5, 13, 17);
+
+insert into chat (mensagem, usuario_id) values
+('Oi, sempre gostei do Tyler, muito legal ter uma plataforma para conversar sobre ele.', 1),
+('Oiii, boa noite gente, sou novo aqui!', 2),
+('Oii, seja bem vindo, qual o album favorito de vocês?', 3),
+('O meu é o Igor', 4),
+('Gosto mais do novo Dont Tap The Glass', 5);
+
+select * from usuario;
+select * from tentativaQuestionario;
+select * from chat;
+
+CREATE VIEW rankingTentativas AS
 SELECT u.nickname, MAX(t.acertos) AS pontuacao
         FROM tentativaQuestionario t
         JOIN usuario u ON t.usuario_id = u.id
         GROUP BY t.usuario_id, u.nickname
         ORDER BY pontuacao DESC
         LIMIT 8;
-        
-        
+
+SELECT * FROM rankingTentativas;
+
+select nickname,
+		acertos,
+        erros,
+        dtRegistro
+from tentativaQuestionario
+join usuario on usuario.id = usuario_id;
+
+select nickname, mensagem
+from usuario
+join chat on usuario.id = usuario_id
+order by usuario.id asc;
