7c8fbbb32da491c24caca22254a84d8e382e6e4b
vicraffa <victor.loliveira@sptech.school>
2025-11-23 04:51:39 -0300

    banco atualizado para nova regra de negocio

--- src/database/Banco de Dados/project-tyler.sql
+++ src/database/Banco de Dados/project-tyler.sql
@@ -14,17 +14,17 @@ create table tentativaQuestionario (
 id int auto_increment,
 usuario_id int,
 primary key (id, usuario_id),
-constraint usuarioTentativaQuestionario foreign key (usuario_id) references usuario (id),
+foreign key (usuario_id) references usuario (id),
 acertos int not null,
 erros int not null,
 dtRegistro datetime default current_timestamp
 );
 
-create table chat(
+create table mensagem(
 id int primary key auto_increment,
-mensagem varchar(200),
+mensagem varchar(450) not null,
 usuario_id int,
-constraint fkUsuarioChat foreign key (usuario_id) references usuario(id)
+foreign key (usuario_id) references usuario(id)
 );
 
 insert into usuario (nickname, email, senha) values
@@ -32,7 +32,11 @@ insert into usuario (nickname, email, senha) values
 ('akira', 'akira@email.com', 'Akira@12345'),
 ('victor', 'victor@email.com', 'Victor@12345'),
 ('nicole', 'nicole@email.com', 'Nicole@12345'),
-('arthur', 'arthur@email.com', 'Arthur@12345');
+('arthur', 'arthur@email.com', 'Arthur@12345'),
+('clederson', 'clederson@email.com', 'Clederson@12345'),
+('jonas', 'jonas@email.com', 'Jonas@12345'),
+('plederson', 'plederson@email.com', 'Plederson@12345'),
+('igonas', 'igonas@email.com', 'igonas@12345');
 
 insert into tentativaQuestionario(usuario_id, acertos, erros) values
 (1, 13, 17),
@@ -41,9 +45,12 @@ insert into tentativaQuestionario(usuario_id, acertos, erros) values
 (3, 10, 20),
 (4, 24, 6),
 (4, 5, 25),
-(5, 13, 17);
+(6, 2, 4),
+(7, 3, 6),
+(8, 5, 8),
+(9, 14, 17);
 
-insert into chat (mensagem, usuario_id) values
+insert into mensagem ( mensagem, usuario_id) values
 ('Oi, sempre gostei do Tyler, muito legal ter uma plataforma para conversar sobre ele.', 1),
 ('Oiii, boa noite gente, sou novo aqui!', 2),
 ('Oii, seja bem vindo, qual o album favorito de vocês?', 3),
@@ -52,17 +59,8 @@ insert into chat (mensagem, usuario_id) values
 
 select * from usuario;
 select * from tentativaQuestionario;
-select * from chat;
+select * from mensagem;
 
-CREATE VIEW rankingTentativas AS
-SELECT u.nickname, MAX(t.acertos) AS pontuacao
-        FROM tentativaQuestionario t
-        JOIN usuario u ON t.usuario_id = u.id
-        GROUP BY t.usuario_id, u.nickname
-        ORDER BY pontuacao DESC
-        LIMIT 8;
-
-SELECT * FROM rankingTentativas;
 
 select nickname,
 		acertos,
@@ -71,7 +69,33 @@ select nickname,
 from tentativaQuestionario
 join usuario on usuario.id = usuario_id;
 
-select nickname, mensagem
-from usuario
-join chat on usuario.id = usuario_id
-order by usuario.id asc;
+create view exibirKpis as
+select 	usuario_id as `user`,
+		count(usuario_id) as quantidadeTentativas, 
+		sum(erros) as quantidadeErros, 
+        sum(acertos) as quantidadeAcertos, 
+        round(sum(acertos) / sum(acertos + erros), 2)  as taxaAcerto
+from tentativaQuestionario
+group by (usuario_id);
+
+select * from exibirKpis
+where user = 5;
+
+create view rankingTentativas as
+select u.nickname, MAX(t.acertos) as pontuacao
+        from tentativaQuestionario t
+        join usuario u on t.usuario_id = u.id
+        group by t.usuario_id, u.nickname
+        order by pontuacao desc
+        limit 8;
+        
+select * from rankingTentativas;
+
+select acertos, dtRegistro from tentativaQuestionario
+where usuario_id = 5
+order by acertos desc
+limit 5;
+
+select nickname, mensagem from mensagem
+join usuario on usuario.id = usuario_id
+order by mensagem.id;
